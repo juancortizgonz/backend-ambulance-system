2d394f94fc22b364b1907c23f4d61af320ba43ae
Juan Camilo Ortiz <juancortizgonz@gmail.com>
2025-02-04 09:58:47 -0500

    Update ambulance CRUD to fix bug in ambulance's position update

--- api/urls.py
+++ api/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
 
     path("ambulances/", views.AmbulanceList.as_view(), name="ambulance-list"),
     path("ambulances/", views.CreateAmbulance.as_view(), name="ambulance-create"),
-    path("ambulances/<int:pk>/", views.AmbulanceRUD.as_view(), name="ambulance-rud"),
+    path("ambulances/<int:pk>/", views.AmbulanceDetailUpdateDelete.as_view(), name="ambulance-rud"),
 
     path("accident-reports/", views.CreateAccidentReport.as_view(), name="accident-report-create"),
     path("accident-reports/", views.AccidentReportList.as_view(), name="accident-report-list"),
--- api/views.py
+++ api/views.py
@@ -1,8 +1,11 @@
 from .serializers import AdminSerializer, HospitalSerializer, AmbulanceSerializer, PatientSerializer, AccidentReportSerializer
 from .models import Admin, Hospital, Ambulance, Patient, AccidentReport
 from rest_framework import generics
+from rest_framework.views import APIView
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework import status
 from django.contrib.auth.decorators import user_passes_test
 from authenticationapi.permissions import CanViewAdmin, CanViewHospital, CanViewAmbulance, CanViewPatient
 
@@ -35,13 +38,35 @@ class CreateAmbulance(generics.ListCreateAPIView):
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
 
-class AmbulanceRUD(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Ambulance.objects.all()
-    serializer_class = AmbulanceSerializer
+class AmbulanceDetailUpdateDelete(APIView):
     permission_classes = [IsAuthenticated]
 
-    def perform_create(self, serializer):
-        serializer.save(user=self.request.user)
+    def get(self, request, pk):
+        # This pk is the ambulance's id
+        ambulance = Ambulance.objects.get(pk=pk)
+        serializer = AmbulanceSerializer(ambulance)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def delete(self, request, pk):
+        ambulance = Ambulance.objects.get(pk=pk)
+        ambulance.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+    def put(self, request, pk):
+        ambulance = Ambulance.objects.get(user=pk)
+        serializer = AmbulanceSerializer(ambulance, data=request.data, partial=False)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def patch(self, request, pk):
+        ambulance = Ambulance.objects.get(user=pk)
+        serializer = AmbulanceSerializer(ambulance, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 # Patient operations
 class PatientList(generics.ListAPIView):
