9ab601edd5959eec38fdfce9445475636c6248f5
Mici7120 <chvilla7120@gmail.com>
2025-11-23 22:56:14 -0500

    feat: add hospital user creation endpoint and serializers for user and hospital

--- api/serializers.py
+++ api/serializers.py
@@ -1,5 +1,31 @@
 from rest_framework import serializers
 from .models import Admin, Hospital, Ambulance, Patient, AccidentReport
+from django.contrib.auth.models import User, Group
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('username', 'password', 'email')
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        user = User.objects.create_user(**validated_data)
+        return user
+
+class HospitalCreationSerializer(serializers.ModelSerializer):
+    user = UserSerializer()
+
+    class Meta:
+        model = Hospital
+        fields = ('user', 'name', 'level', 'classification', 'address', 'phone_number', 'bed_capacity', 'is_available', 'latitude', 'longitude', 'description')
+
+    def create(self, validated_data):
+        user_data = validated_data.pop('user')
+        user = UserSerializer.create(UserSerializer(), validated_data=user_data)
+        hospital_group, created = Group.objects.get_or_create(name='Hospital')
+        user.groups.add(hospital_group)
+        hospital = Hospital.objects.create(user=user, **validated_data)
+        return hospital
 
 class AdminSerializer(serializers.ModelSerializer):
     class Meta:
